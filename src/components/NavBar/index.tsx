ee095a6d5a663d9c417f707fe0746e1fb4d33b4c
Andrew Marek <andwmarek@gmail.com>
2024-04-11 21:50:47 -0700

    add sticky navbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -10,7 +10,7 @@ export default function NavBar(): React.JSX.Element {
   ];
 
   return (
-    <nav className="bg-raisin-black p-4 w-full">
+    <nav className="sticky top-0 bg-raisin-black p-4 w-full">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <div>
           <Link className="font-sans text-2xl text-champagne-pink hover:text-papaya-whip hover:drop-shadow-glow font-bold transition-all duration-500" href="/">
